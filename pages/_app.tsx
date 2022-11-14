ea2b5093176ddebb81e58db5726e0b2ede999901
Zachary Truong <zacharytruong@gmail.com>
2022-11-13 23:17:50 -0600

    added ChakraProvider and theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,39 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import 'reset-css';
+
+const theme = extendTheme({
+  colors: {
+    gray: {
+      100: '#f5f5f5',
+      200: '#eee',
+      300: '#e0e0e0',
+      400: '#bdbdbd',
+      500: '#9e9e9e',
+      600: '#757575',
+      700: '#616161',
+      800: '#242424',
+      900: '#212121'
+    }
+  },
+  components: {
+    Button: {
+      variant: {
+        link: {
+          ':focus': {
+            outline: 'none',
+            boxShadow: 'none'
+          }
+        }
+      }
+    }
+  }
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
